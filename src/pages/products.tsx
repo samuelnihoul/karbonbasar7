15fde6f5e984b929ec0bb4aefdeca7f3e4fc9c96
Bigandy4climate <samuelnihoul@hotmail.fr>
2023-07-21 11:29:32 +0200

    Revert "Merge branch 'main' of https://github.com/samuelnihoul/karbonbasar7"
    This reverts commit 6ec3e8dbaf95ba903d43712295b03622484cd93b, reversing
    changes made to 8efa08306336ee4def3a486d069de955073cb5c8.

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from 'react';
 import Grid from '@mui/material/Grid';
 import Product from '../components/Product';
 import { db } from '../lib/firebase'
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, getDocs, DocumentData } from 'firebase/firestore'
 import Counter from '../components/Counter';
 import { useTranslation } from 'react-i18next'
 // transfer the price to the checkout page using React state
 const Products = () => {
-  const { t } = useTranslation(["offset"])
-  const [products, setProducts] = useState([])
+    const { t } = useTranslation(["offset"])
+  const [products, setProducts] = useState<DocumentData[]>([])
   //fetch from cloud firestore
   async function fetchProducts() {
     const snapshot = await getDocs(collection(db, 'products'))
@@ -26,17 +26,17 @@ const Products = () => {
   }, [])
   return (
     <>
-      <section>
+    <section>
 
-        <h2 className='center text-xl mt-[2.5vh] mb-[2.5vh]'>{t('ourprojects')}</h2><Grid container spacing={4}>
-          {products.map((product) => (
-            <Grid key={product.name} item xs={12} sm={6} md={4} lg={3}>
-              <Product product={product} />
-            </Grid>
-          ))}
-        </Grid>
-        <Counter  ></Counter>
-      </section>
+      <h2 className='center text-xl mt-[2.5vh] mb-[2.5vh]'>{t('ourprojects')}</h2><Grid container spacing={4}>
+        {products.map((product) => (
+          <Grid key={product.name} item xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
+      <Counter  ></Counter>
+    </section>
     </>
   );
 };
