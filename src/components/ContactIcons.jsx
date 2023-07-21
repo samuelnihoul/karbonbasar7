50a9b7756e9314ca5450e2ba11fd0a914e15f064
Bigandy4climate <samuelnihoul@hotmail.fr>
2023-07-21 13:51:14 +0200

    purified the corporate page

--- src/components/ContactIcons.jsx
+++ src/components/ContactIcons.jsx
@@ -1,33 +1,6 @@
 import { createStyles, ThemeIcon, Text, SimpleGrid, Box, Stack } from '@mantine/core';
 import { IconSun, IconPhone, IconMapPin, IconAt } from '@tabler/icons-react';
 
-const useStyles = createStyles((theme, { variant }) => ({
-  wrapper: {
-    display: 'flex',
-    alignItems: 'center',
-    color: theme.white,
-  },
-
-  icon: {
-    marginRight: theme.spacing.md,
-    backgroundImage:
-      variant === 'gradient'
-        ? `linear-gradient(135deg, ${theme.colors[theme.primaryColor][4]} 0%, ${theme.colors[theme.primaryColor][6]
-        } 100%)`
-        : 'none',
-    backgroundColor: 'transparent',
-  },
-
-  title: {
-    color: variant === 'gradient' ? theme.colors.gray[6] : theme.colors[theme.primaryColor][0],
-  },
-
-  description: {
-    color: variant === 'gradient' ? theme.black : theme.white,
-  },
-}));
-
-
 function ContactIcon({
   icon: Icon,
   title,
@@ -36,38 +9,30 @@ function ContactIcon({
   className,
   ...others
 }) {
-  const { classes, cx } = useStyles({ variant });
   return (
-    <div className={cx(classes.wrapper, className)} {...others}>
-      {variant === 'gradient' ? (
-        <ThemeIcon size={40} radius="md" className={classes.icon}>
-          <Icon size="1.5rem" />
-        </ThemeIcon>
-      ) : (
-        <Box mr="md">
-          <Icon size="1.5rem" />
-        </Box>
-      )}
-
+    <div >
+      <Box mr="md">
+        <Icon size="1.5rem" />
+      </Box>
       <div>
-        <Text size="xs" className={classes.title}>
+        <Text size="xs">
           {title}
         </Text>
-        <Text className={classes.description}>{description}</Text>
+        <Text>{description}</Text>
       </div>
     </div>
   );
 }
 
 
-const MOCKDATA = [
+const DATA = [
   { title: 'Email', description: 'contact@harmonia.eco', icon: IconAt },
   { title: 'Phone', description: '+33 782 949 579', icon: IconPhone },
-  { title: 'Address', description: '14 La Blaye - 22150 Ploeuc-l\'Hermitage', icon: IconMapPin },
+  { title: 'Address', description: '14 La Blaye - 22150 Ploeuc-l\'Hermitage - FR', icon: IconMapPin },
   { title: 'Working hours', description: '8 a.m. – 5 p.m.', icon: IconSun },
 ];
 
-export function ContactIconsList({ data = MOCKDATA, variant }) {
+export function ContactIconsList({ data = DATA, variant }) {
   const items = data.map((item, index) => <ContactIcon key={index} variant={variant} {...item} />);
   return <Stack>{items}</Stack>;
 }
@@ -76,24 +41,13 @@ export function ContactIcons() {
   return (
     <SimpleGrid cols={2} breakpoints={[{ maxWidth: 755, cols: 1 }]}>
       <Box
-        sx={(theme) => ({
-          padding: theme.spacing.xl,
-          borderRadius: theme.radius.md,
-          backgroundColor: theme.white,
-        })}
       >
         <ContactIconsList />
       </Box>
 
       <Box
-        sx={(theme) => ({
-          padding: theme.spacing.xl,
-          borderRadius: theme.radius.md,
-          backgroundImage: `linear-gradient(135deg, ${theme.colors[theme.primaryColor][6]} 0%, ${theme.colors[theme.primaryColor][4]
-            } 100%)`,
-        })}
       >
-        <ContactIconsList variant="white" />
+        <ContactIconsList variant="gradient" />
       </Box>
     </SimpleGrid>
   );
