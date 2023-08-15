4273d471fcbb29fa2e698ae19fa58e4fa457cc4e
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2023-08-15 17:45:00 +0200

    put the provider - getting u is not defined webpack error

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './pages/_app.jsx';
+import { HashConnectAPIProvider } from './lib/hashconnect.tsx';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
-);
+    <HashConnectAPIProvider>
+        <App /></HashConnectAPIProvider>
+); u
--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -14,6 +14,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-getAnalytics(app);
 
 export { db };
