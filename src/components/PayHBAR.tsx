20203894cde2399ab8e0c72d4d7fe892c6a96ffb
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2023-08-17 14:28:52 +0200

    all good and snackbars

--- src/components/PayHBAR.tsx
+++ src/components/PayHBAR.tsx
@@ -1,9 +1,11 @@
 import { Hbar, TransferTransaction } from "@hashgraph/sdk";
 import {
+    Box,
     Stack,
     Typography,
     Button,
     Select,
+    Snackbar,
     MenuItem,
     TextField,
 } from "@mui/material";
@@ -16,16 +18,18 @@ interface Props {
     price: number
 }
 export default function PayHBAR({ quantity, price }: Props) {
+    const [snackbarOpen, setSnackbarOpen] = useState(false)
     const { accountIds: connectedAccountIds, isConnected } = useSelector(
         (state: AppStore) => state.hashconnect
     );
 
     const [fromAccountId, setFromAccountId] = useState("");
     const [toAccountId, setToAccountId] = useState("");
-
+    const [snackbarMessage, setSnackbarMessage] = useState("")
+    const amount = Math.floor(quantity * price * 1000 / 0.063) / 1000
     return (
         <Stack maxWidth="400px" spacing={1} pt={8}>
-            <Typography variant="h3">Buy for {price * quantity} HBAR</Typography>
+            <Typography variant="h3">Buy for {amount} HBAR</Typography>
             <Typography>From Account ID:</Typography>
             <Select
                 color={"blurple" as any}
@@ -67,19 +71,41 @@ export default function PayHBAR({ quantity, price }: Props) {
                 variant="contained"
                 color={"blurple" as any}
                 onClick={async () => {
-                    const amount = Math.floor(quantity * price * 1000 / 0.063) / 1000
                     const transferTransaction = new TransferTransaction()
                         .addHbarTransfer(fromAccountId, new Hbar(-amount))
                         .addHbarTransfer('0.0.1082962', new Hbar(amount))
                         .setTransactionMemo(toAccountId)
+                    setSnackbarMessage("Approve in your wallet.")
+                    setSnackbarOpen(true)
                     const signer = await getSigner(fromAccountId);
                     const frozenTransaction =
                         await transferTransaction.freezeWithSigner(signer);
                     await frozenTransaction.executeWithSigner(signer);
+                    setSnackbarMessage("Success!")
+                    setSnackbarOpen(true)
                 }}
             >
                 Send
-            </Button>
+            </Button><Snackbar
+                anchorOrigin={{ vertical: "top", horizontal: "center" }}
+                open={snackbarOpen}
+                autoHideDuration={5000}
+                onClose={() => {
+                    setSnackbarOpen(false);
+                }}
+            >
+                <Box
+                    sx={{
+                        bgcolor: "success.main",
+                        color: "white",
+                        p: 2,
+                        borderRadius: 1,
+                    }}
+
+                >
+                    {snackbarMessage}
+                </Box>
+            </Snackbar>
         </Stack>
     )
 }
