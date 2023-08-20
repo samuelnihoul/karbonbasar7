f013b8da87f2914c1045dd2a2d9af66cb187dc28
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2023-08-20 14:18:30 +0200

    rm analytics

--- src/index.jsx
+++ src/index.jsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     // <>
     <React.StrictMode>
-        <Analytics />
+        // <Analytics />
         <Provider store={store}>
             <HashConnectClient />
             <ThemeProvider>
