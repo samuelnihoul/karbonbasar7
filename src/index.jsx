ab1162e6bbd277b5e1b733d6dcaf79f4155ae0c8
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2023-09-11 21:47:46 +0200

    moved dpd

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom/client';
 import App from './pages/_app';
-import { HashConnectClient } from './components/HashButton.tsx';
+import { HashConnectClient } from './components/HashButton';
 import { store } from './store'
 import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from './theme'
