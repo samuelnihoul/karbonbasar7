5f6528722e9d665881193ab587db43ea0e9dfb92
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2023-09-11 21:25:29 +0200

    created the new pages

---
+++ src/pages/governance.tsx
@@ -0,0 +1,10 @@
+export default () => {
+
+    return (
+        <section>
+            <p>
+                Karbon Moneta also serves as our governance token. Stay tune for the first improvement proposals and votes.
+            </p>
+        </section>
+    )
+}
---
+++ src/pages/karbonmoneta.tsx
@@ -0,0 +1,10 @@
+export default () => {
+
+    return (
+        <section>
+            <p>
+                harmonia-eko's token for Karbon Basar and beyond. Reduce emissions and get rewarded. In active development. Join our Discord to get the latest news and to get more information. You may currently get a few KM by purchasing emision reductions at karbonbasar.harmonia.eco/reductions.
+            </p>
+        </section>
+    )
+}
