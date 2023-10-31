0cccd52c2112160b4642ce0780b14f662861d005
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2023-10-31 10:19:03 +0000

    i18n.ts

---
+++ app/i18n.ts
@@ -0,0 +1,22 @@
+import i18n from 'i18next'
+import Backend from 'i18next-http-backend'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import { initReactI18next } from 'react-i18next'
+i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init(() => (
+  {
+    backend: {
+      loadPath: "/assets/i18n/{{ns}}/{{lng}}.json"
+    },
+    debug: false,
+    fallbackLng: "en",
+    ns: ['common', 'home', 'checkout', 'about', 'offset'],
+    interpolation: {
+      espaceValue: false,
+      formatSeparation: ','
+    },
+    react: {
+      useSuspense: true,
+    },
+  }
+))
+export default i18n;
