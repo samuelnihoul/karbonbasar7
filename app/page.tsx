38c0d215f5dad670d7767c1e3402de892a82d68c
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2023-11-06 18:24:42 +0000

    moved the component folder

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+'use client'
+import React from 'react';
+import Counter from '../components/Counter'
+import HowItWorks from "../components/HowItWorks";
+import NFTs from "../components/NFTs.jsx";
+import { useTranslation } from 'react-i18next'
+export default function Home() {
+    const { t } = useTranslation(["home"])
+    return (
+        <>
+            <h1 className="text-center text-3xl mt-[10vh] mb-[10vh]">
+                KARBON BASAR
+            </h1>
+            <p className="text-center text-xl">
+                {t('Cute Collectible Emission Reductions')}
+                <br />
+                {t('And More')}
+                <br />
+                ฅ՞•ﻌ•՞ฅ
+            </p>
+            <NFTs />
+            <HowItWorks />
+            <Counter />
+        </>
+    )
+}
