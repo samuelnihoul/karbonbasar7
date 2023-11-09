1bead1d405c765734c27dfbf60aa88564fd57e50
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2023-11-09 18:15:25 +0000

    dev dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,17 @@
-'use client'
 import React from 'react';
 import Counter from '../components/Counter'
 import HowItWorks from "../components/HowItWorks";
 import NFTs from "../components/NFTs";
-import { useTranslation } from 'react-i18next'
 export default function Home() {
-    const { t } = useTranslation(["home"])
     return (
         <>
             <h1 className="text-center text-3xl mt-[10vh] mb-[10vh]">
                 KARBON BASAR
             </h1>
             <p className="text-center text-xl">
-                {t('Cute Collectible Emission Reductions')}
+            Hello
                 <br />
-                {t('And More')}
+                2
                 <br />
                 ฅ՞•ﻌ•՞ฅ
             </p>
