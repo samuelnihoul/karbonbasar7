3acd8f95b0bd8ac17df394a9a49c1ec72f9cc5fc
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2023-11-11 15:06:57 +0000

    createContext error solved

--- components/HowItWorks.tsx
+++ components/HowItWorks.tsx
@@ -4,9 +4,9 @@ export default function HowItWorks() {
     return (
         <section className='text-center'>
             <h2 className='mb-[2.5vh] mt-[2.5vh] text-2xl'>{'How It Works'}</h2>
-            <p className='p-2 pb-[50px]'>{t('On Karbon Basar, you can find a selection of most efficient emission reduction projects from across the globe.')}</p>
+            <p className='p-2 pb-[50px]'>{'On Karbon Basar, you can find a selection of most efficient emission reduction projects from across the globe.'}</p>
             <img src='/assets/images/example2.png' className='m-auto'></img>
-            <a href='/reductions' className='mt-[12px] text-l'>{t('tryit')}</a>
+            <a href='/reductions' className='mt-[12px] text-l'>{'Try It'}</a>
         </section >
     )
 }
