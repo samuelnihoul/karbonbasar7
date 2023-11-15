46601627c69a2b1acedd9da0991f9f34946671ba
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2023-11-15 06:37:14 +0000

    package.json tweaks

--- tests/hashpackConnection.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test'
-test('has title', async ({ page }) => {
-    await page.goto('https://webapp.hashpack.app');
-
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle();
-});
