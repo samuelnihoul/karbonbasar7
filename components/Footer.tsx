cfb98675b7123eb2602bfbe1d4c347f005c2b745
Samuel Nihoul <samuelnihoul@tutanota.com>
2023-11-18 14:50:09 +0100

    ready for merge again

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,35 +1,35 @@
 import React from 'react'
 import footerData from '../data/footer-data'
 import socialData from "../data/social-data"
-export default function Footer  ()  {
+export default function Footer() {
   return (
-    <section>
-      <ul>
-     {
-     footerData.map(
-     (el)=>{
-     return(<li key={el.name}>
-     <a href={el.link}>
-     {el.name}
-     </a>
-     </li>)
-     }
-         )
-     } 
+    <section className='flex flex-row'>
+      <ul className='flex flex-row flex-grow gap-[2rem] m-[2rem]'>
+        {
+          footerData.map(
+            (el) => {
+              return (<li key={el.name}>
+                <a href={el.link}>
+                  {el.name}
+                </a>
+              </li>)
+            }
+          )
+        }
       </ul>
-      <ul>
-     {
-     socialData.map(
-     (el)=>{
-     return(
-     <li key={el.icon}>
-     <a href={el.link}>
-     {el.icon}
-     </a>
-     </li>)
-     }
-         )
-     } 
+      <ul className='flex flex-row gap-[2rem] m-[2rem]'>
+        {
+          socialData.map(
+            (el) => {
+              return (
+                <li key={el.icon}>
+                  <a href={el.link}>
+                    {el.icon}
+                  </a>
+                </li>)
+            }
+          )
+        }
       </ul>
     </section>
   )
