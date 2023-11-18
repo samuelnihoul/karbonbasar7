730d8fe4d5f4ce150e77af092309bef47bb6a2c1
Samuel Nihoul <samuelnihoul@tutanota.com>
2023-11-18 17:58:34 +0100

    tags

--- components/Product.tsx
+++ components/Product.tsx
@@ -15,9 +15,9 @@ export default function Product({ product }: { product: ProductInterface }) {
       < img className="h-[10rem] m-auto rounded-md" src={product.image} alt={product.name.EN} />
       <div>
         <p className="text-[2rem]">{product.name.EN}</p>
-        <span className="p-10 text-m">${product.price}/0.1 CO2e tonne</span>
+        <span className="p-10 text-m">${product.price}/0.1 CO2e ton</span>
         <div className="my-4">
-          <span className="mainColor">{'Quantity'}</span>
+          <span className="mainColor">{'Quantity: '}</span>
           <input
             className="placeholder-black text-black"
             type="number"
@@ -25,19 +25,16 @@ export default function Product({ product }: { product: ProductInterface }) {
             onChange={handleQuantityChange}
           />
         </div>
-
         <div>
           {product.description.FR}
         </div>
-
         <div className="my-4">
-          {product.tags.map(chip => <span key={chip} >{chip}</span>)}
+          {product.tags.map(chip => <span key={chip} className='rounded-full bg-white m-[1rem] px-[1rem] py-[0.5rem]'>{chip}</span>)}
         </div>
-
         <div>
-          <span className="mainColor">{'Available Stock'}</span> {product.stock}
+          <span className="mainColor">{'Available Stock: '}</span> {product.stock}
           <br />
-          <span className="mainColor">{'Methodology'}</span> {product.methodology}
+          <span className="mainColor">{'Methodology: '}</span> {product.methodology}
         </div>
       </div>
       <div >
