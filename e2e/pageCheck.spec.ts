13ecbc54ec1089f26fa7e814d9940be532559bc3
Samuel Nihoul <samuelnihoul@tutanota.com>
2023-11-19 16:17:39 +0100

    Revert "screensdot test"
    This reverts commit 6e637583d3507af2ae334cd63fc3edc50b8dd263.

--- e2e/pageCheck.spec.ts
+++ e2e/pageCheck.spec.ts
@@ -1,10 +1,11 @@
 import { test, expect } from '@playwright/test'
 
-const routes = ['/', '/about', '/contact', '/governance', '/privacy', '/products', '/terms'];
+const routes = ['/', '/about', '/contact','/governance','/privacy','/products','/terms'];
 
 for (const route of routes) {
   test(`page "${route}" should load without error`, async ({ page }) => {
     await page.goto(`http://localhost:3000${route}`);
-    await expect(page).toHaveScreenshot()
+    const isPageLoaded = await page.waitForSelector('body');
+    expect(isPageLoaded).toBeTruthy();
   });
 }
