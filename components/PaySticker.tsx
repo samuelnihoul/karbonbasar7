31635fce0024a50525fbde67d38136dbe8a712f4
Samuel Nihoul <samuelnihoul@tutanota.com>
2023-11-23 19:26:45 +0100

    form template

--- components/PaySticker.tsx
+++ components/PaySticker.tsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export default function PaySticker() {
     return (
-        <div>PaySticker</div>
+        <>
+            <form action="/" className='flex flex-col gap-[0.5rem]'>
+                <label htmlFor="address">Address</label>
+                <input type="text" id='address' />
+                <label htmlFor="zip">ZIP</label>
+                <input type="text" id="zip" />
+                <label htmlFor="city">City</label>
+                <input type="text" />
+                <label htmlFor="country">Country</label>
+                <input type="text" id="country" />
+                <label htmlFor="email">Email</label>
+                <input type="text" id="email" />
+            </form>
+        </>
     )
 }
