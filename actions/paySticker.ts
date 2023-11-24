e087146cabe3ff04ac1f5e82912d027578d7aa6a
Samuel Nihoul <samuelnihoul@tutanota.com>
2023-11-24 17:40:19 +0100

    moved the action to a separate file

---
+++ actions/paySticker.ts
@@ -0,0 +1,18 @@
+'use server'
+import db from '@/lib/firebase'
+import { addDoc, collection } from 'firebase/firestore'
+import schema from '@/schema/address'
+export default async function (data: FormData) {
+    const address = data.get('address')
+    const zip = data.get('zip')
+    const city = data.get('city')
+    const country = data.get('country')
+    const email = data.get('email')
+    addDoc(collection(db, 'purchases'), {
+        'address': address,
+        'zip': zip,
+        'city': city,
+        'country': country,
+        'email': email
+    })
+}
--- components/PaySticker.tsx
+++ components/PaySticker.tsx
@@ -1,25 +1,9 @@
 import React from 'react'
-import db from '@/lib/firebase'
-import { addDoc, collection } from 'firebase/firestore'
-async function post(data: FormData) {
-    'use server'
-    const address = data.get('address')
-    const zip = data.get('zip')
-    const city = data.get('city')
-    const country = data.get('country')
-    const email = data.get('email')
-    addDoc(collection(db, 'purchases'), {
-        'address': address,
-        'zip': zip,
-        'city': city,
-        'country': country,
-        'email': email
-    })
-}
+import action from '@/actions/paySticker'
 export default function PaySticker() {
     return (
         <>
-            <form action={post} className='flex flex-col gap-[0.5rem]'>
+            <form action={action} className='flex flex-col gap-[0.5rem]'>
                 <label htmlFor="address" >Address</label>
                 <input type="text" name='address' id='address' />
                 <label htmlFor="zip">ZIP</label>
