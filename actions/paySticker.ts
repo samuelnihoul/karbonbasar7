6b28ba35e1772ff69bc427650397a200b5e41ca4
Samuel Nihoul <samuelnihoul@tutanota.com>
2023-11-24 17:55:25 +0100

    fixed the compiler options

--- actions/paySticker.ts
+++ actions/paySticker.ts
@@ -1,7 +1,7 @@
 'use server'
 import db from '@/lib/firebase'
 import { addDoc, collection } from 'firebase/firestore'
-import schema from '@/schema/address'
+import schema from '@/schemas/address'
 export default async function (data: FormData) {
     const address = data.get('address')
     const zip = data.get('zip')
