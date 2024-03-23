c29cbdc97f1d3d0e6c08c975786a15b9ccb2ba77
Samuel Nihoul <samuelnihoul@tutanota.com>
2024-03-23 18:50:13 +0100

    updated dependencies

--- app/paypal/utility.ts
+++ app/paypal/utility.ts
@@ -3,12 +3,13 @@ import paypal from '@paypal/checkout-server-sdk'
 const configureEnvironment = function () {
     const clientId = process.env.PAYPAL_ID
     const clientSecret = process.env.PAYPAL_SECRET
-
-    console.log("paypla clientId is" + clientId)
+    const a = process.env.PAYPAL_SANDBOX
+    const b = process.env.PAYPAL_SECRET_SANDBOX
+    console.log("paypal clientId is" + clientId)
     console.log("paypal clientSecret is" + clientSecret)
     return process.env.NODE_ENV === 'production'
         ? new paypal.core.LiveEnvironment(clientId, clientSecret)
-        : new paypal.core.SandboxEnvironment(clientId, clientSecret)
+        : new paypal.core.SandboxEnvironment(a, b)
 }
 
 const client = function () {
